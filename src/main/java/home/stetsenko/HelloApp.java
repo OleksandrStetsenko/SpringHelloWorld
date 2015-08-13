a278451489158cff4bc1013cc1e216c1d5940418
OleksandrStetsenko <stetsenko2004@gmail.com>
2015-08-13 23:08:41 +0300

    junit added

--- src/main/java/home/stetsenko/HelloApp.java
+++ src/main/java/home/stetsenko/HelloApp.java
@@ -6,8 +6,6 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloApp {
     public static void main(String[] args) {
-        //ApplicationContext context = new ClassPathXmlApplicationContext("app-context-xml.xml");
-        //MessageRenderer renderer = context.getBean("renderer", MessageRenderer.class);
         GenericXmlApplicationContext context = new GenericXmlApplicationContext();
         context.load("app-context-annotation.xml");
         context.refresh();
---
+++ src/test/java/home/stetsenko/CoreTests.java
@@ -0,0 +1,45 @@
+package home.stetsenko;
+
+import org.junit.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class CoreTests {
+
+    private static final String XML_CONSTRACTOR_MESSAGE = "This is constructor message - xml";
+    private static final String ANNOTATION_CONSTRACTOR_MESSAGE = "This is constructor message - annotation";
+
+    @Test
+    public void testXML() {
+        GenericXmlApplicationContext context = getContext("app-context-xml.xml");
+
+        MessageRenderer renderer = context.getBean("messageRenderer", MessageRenderer.class);
+        MessageProvider provider = renderer.getMessageProvider();
+        assertNotNull(provider);
+        String message = provider.getMessage();
+        assertNotNull(message);
+        assertEquals(message, XML_CONSTRACTOR_MESSAGE);
+    }
+
+    @Test
+    public void testAnnotation() {
+        GenericXmlApplicationContext context = getContext("app-context-annotation.xml");
+
+        MessageRenderer renderer = context.getBean("messageRenderer", MessageRenderer.class);
+        MessageProvider provider = renderer.getMessageProvider();
+        assertNotNull(provider);
+        String message = provider.getMessage();
+        assertNotNull(message);
+        assertEquals(message, ANNOTATION_CONSTRACTOR_MESSAGE);
+    }
+
+    private GenericXmlApplicationContext getContext(String s) {
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext();
+        context.load(s);
+        context.refresh();
+        return context;
+    }
+
+}
